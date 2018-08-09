6201f9673c62f7a11cb97de7f13730f20a06ccb1
Dan Mace and Eric Conner <dan@>
2018-08-09 10:45:33 -0700

    add form check branch

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,16 @@ $(function() {
   $("form#height-check").submit(function(event) {
     event.preventDefault();
     var hInput = parseInt($("input#height-input").val());
-    console.log(hInput);
+  if(hInput) {
     if (hInput < 52) {
       $('.short').show();
     } else if (hInput > 70) {
       $('.tall, .short').show();
     } else {
       $('.tall, .short, .tootall').show();
+
+    }} else {
+      alert('Please enter your height')
     }
   });
 });
